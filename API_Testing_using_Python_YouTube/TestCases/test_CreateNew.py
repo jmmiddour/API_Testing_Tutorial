ee6094d155d188ca3b5167c2ce8f7cd70ac65180
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-18 16:13:49 -0400

    Completed video 6

---
+++ API_Testing_using_Python_YouTube/TestCases/test_CreateNew.py
@@ -0,0 +1,54 @@
+"""
+This file contains logic for testing a Post request using Pytest.
+"""
+
+######################################################################################################################
+# ########################################### Video 6 Code and Notes ############################################### #
+######################################################################################################################
+
+# Imports
+import requests
+import json
+import jsonpath
+
+# Api URL - POST Request
+url = 'https://reqres.in/api/users'
+
+
+def test_create_new():
+    # Open the json file - need to use .. in front of / because I am going
+    #   one directory up from the current directory
+    file = open('../CreateUser.json', 'r')
+
+    # Read in the json file - returns a string
+    json_in = file.read()
+
+    # Load the json file - parses the string into json format
+    request_json = json.loads(json_in)
+
+    # Display the json file to verify it is loaded in properly
+    print(f'JSON File Imported: {request_json}')
+
+    # Send the Post request with json input as the response body
+    response = requests.post(url, request_json)
+
+    # Display the content of the response
+    print(f'Response Content: {response.content}')
+
+    # Verify the proper response code is returned
+    assert response.status_code == 201
+
+    # Get the full header data from the response
+    print(f'Full Response Header: {response.headers}')
+
+    # Get just the Content-Length from the header data from the response
+    print(f'Content Length from Header: {response.headers.get("Content-Length")}')
+
+    # Parse response to json format
+    response_json = json.loads(response.text)
+
+    # Get the id from the response
+    resp_id = jsonpath.jsonpath(response_json, 'id')
+
+    # Display the first id in the id list
+    print(f'Id from JSON Response: {resp_id[0]}')
